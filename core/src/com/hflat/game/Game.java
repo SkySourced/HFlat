34003e86212ce11f3c02cfd4e16fb47f75a536e2
SkySourced <58579786+SkySourced@users.noreply.github.com>
2024-02-10 09:09:12 +1300

    draws centred text

--- core/src/com/hflat/game/Game.java
+++ core/src/com/hflat/game/Game.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -36,8 +37,8 @@ public class Game extends ApplicationAdapter {
 		ScreenUtils.clear(1, 1, 1, 1);
 		batch.begin();
 		batch.draw(logo, (float) Gdx.graphics.getWidth() /2 - (float) logo.getWidth() /2, (float) Gdx.graphics.getHeight() /2 - (float) logo.getHeight() /2 + 70);
-		pixelFont.draw(batch, "Uses unlicensed assets!", 50, (float) Gdx.graphics.getHeight() /2 - 50);
-		pixelFont.draw(batch, "Loading...", 140, (float) Gdx.graphics.getHeight() /2 - 80);
+		drawCentredText(batch, pixelFont, "Uses unlicenced assets!", (float) Gdx.graphics.getHeight() /2 - 100);
+		drawCentredText(batch, pixelFont, "Loading...", (float) Gdx.graphics.getHeight() /2 - 150);
 		batch.end();
 	}
 	
@@ -46,5 +47,17 @@ public class Game extends ApplicationAdapter {
 		batch.dispose();
 		logo.dispose();
 		pixelFont.dispose();
+	}
+
+	public static float getCentreTextOffset(BitmapFont font, String text) {
+		GlyphLayout layout = new GlyphLayout();
+		layout.setText(font, text);
+		return layout.width / 2;
+	}
+
+	public static void drawCentredText(SpriteBatch batch, BitmapFont font, String text, float y) {
+		GlyphLayout layout = new GlyphLayout();
+		layout.setText(font, text);
+		font.draw(batch, text, (float) Gdx.graphics.getWidth() /2 - layout.width / 2, y);
 	}
 }
