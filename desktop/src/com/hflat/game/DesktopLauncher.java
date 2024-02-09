4fa710d63a2086f54d93579cb5236e8ac7b78a3f
SkySourced <58579786+SkySourced@users.noreply.github.com>
2024-02-09 23:26:46 +1300

    libgdx boilerplate

---
+++ desktop/src/com/hflat/game/DesktopLauncher.java
@@ -0,0 +1,21 @@
+package com.hflat.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+
+		// remove frame cap
+		config.setForegroundFPS(0);
+		config.useVsync(false);
+
+		// set window size
+		config.setWindowedMode(400, 1080);
+
+		config.setTitle("HFlat");
+		new Lwjgl3Application(new Game(), config);
+	}
+}
