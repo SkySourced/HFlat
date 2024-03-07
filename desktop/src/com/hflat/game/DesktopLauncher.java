2ac9ea4bcc5505e2b6d0b0cc0f26022ab54a1a46
SkySourced <58579786+SkySourced@users.noreply.github.com>
2024-03-07 22:35:42 +1300

    auto scale window

--- desktop/src/com/hflat/game/DesktopLauncher.java
+++ desktop/src/com/hflat/game/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.hflat.game;
 
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
@@ -8,12 +9,18 @@ public class DesktopLauncher {
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 
+        Graphics.Monitor monitor = Lwjgl3ApplicationConfiguration.getPrimaryMonitor();
+        Graphics.DisplayMode desktopMode = Lwjgl3ApplicationConfiguration.getDisplayMode(monitor);
+
+        int height = (int) (desktopMode.height * 0.9);
+        float aspectRatio = 4/7f;
+
         // remove frame cap
         config.setForegroundFPS(0);
         config.useVsync(false);
 
         // set window size
-        config.setWindowedMode(400, 700);
+        config.setWindowedMode((int) (height * aspectRatio), height);
 
         config.setTitle("HFlat");
         new Lwjgl3Application(new HFlatGame(), config);
